e7941b3b5184cac76bc2c975c36abbbbdff648e9
terry5333 <terrylie0215@gmail.com>
2026-02-17 19:20:47 +0800

    更新 commands.js

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,3 +1,14 @@
 "use strict";
 
-module.exports = require("./commands_admin");
+/**
+ * 只處理 admin slash（install/close/ping）
+ * index.js 已經 deferReply(ephemeral)
+ */
+
+const admin = require("./commands_admin");
+
+async function execute(interaction, ctx) {
+  return admin.execute(interaction, ctx);
+}
+
+module.exports = { execute };
